508f3959c0684f759e1a3c4177bd07add716c4de
Cameron Aldren <148953941+cja119@users.noreply.github.com>
2025-01-17 10:47:23 +0000

    Update BuildModel.py

--- ExecutionScripts/BuildModel.py
+++ ExecutionScripts/BuildModel.py
@@ -11,7 +11,6 @@ from OptimisationScripts.OptimisationModel import OptimModel
 from PreOptimisationDataStore.DefaultParameters import Default_Params
 import numpy as np
 
-
 booleans = {'vector_choice':{'LH2':True if sys.argv[3] == 'LH2' else False,
                                 'NH3':True if sys.argv[3] == 'NH3' else False
                                 },
@@ -22,18 +21,21 @@ booleans = {'vector_choice':{'LH2':True if sys.argv[3] == 'LH2' else False,
                 'grid_connection':False,
                 'wind':True,
                 'solar':False,
-                'net_present_value': True}
+                'net_present_value': True,
+                'grid_wheel':False
+             }
 
 parameters = Default_Params().formulation_parameters
 parameters['booleans'] = booleans
-parameters['stage_duration'] = int(sys.argv[5])
-parameters['n_stages'] = int(sys.argv[1])
-parameters['n_stochastics'] = int(sys.argv[2])
-parameters['hydrogen_price'] = 5 #$/kg
-parameters['random_seed'] = int(sys.argv[4])
+parameters['stage_duration'] = 168
+parameters['n_stages'] = 39
+parameters['n_stochastics'] = 1
+parameters['random_seed'] = 42
 parameters['relaxed_ramping'] = True
-parameters['vector_operating_duration'] = 1
-parameters['shipping_decision'] = 168
+parameters['hydrogen_price'] = 5 #$/kg
+parameters['vector_operating_duration']= 1
+parameters['shipping_decision']=168
+parameters['wheel_period']=24
 
 model = OptimModel(parameters, key=sys.argv[6])
 
