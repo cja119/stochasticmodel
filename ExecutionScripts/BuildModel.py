973f5c40758908d8a361f96eba662b832c71202f
Cameron Aldren <148953941+cja119@users.noreply.github.com>
2025-01-17 10:53:42 +0000

    Update BuildModel.py

--- ExecutionScripts/BuildModel.py
+++ ExecutionScripts/BuildModel.py
@@ -18,25 +18,26 @@ booleans = {'vector_choice':{'LH2':True if sys.argv[3] == 'LH2' else False,
                                 'PEM':True,
                                 'SOFC':True
                                 },
-                'grid_connection':False,
-                'wind':True,
-                'solar':False,
+               'grid_connection':True if sys.argv[7]=='True' else False,
+                'wind':sys.argv[6] in ['Wind', 'Both'],
+                'solar':sys.argv[6] in ['Solar', 'Both'],
                 'net_present_value': True,
                 'grid_wheel':False
              }
 
 parameters = Default_Params().formulation_parameters
 parameters['booleans'] = booleans
-parameters['stage_duration'] = 168
-parameters['n_stages'] = 39
-parameters['n_stochastics'] = 1
-parameters['random_seed'] = 42
-parameters['relaxed_ramping'] = True
-parameters['hydrogen_price'] = 5 #$/kg
-parameters['vector_operating_duration']= 1
-parameters['shipping_decision']=168
-parameters['wheel_period']=24
 
-model = OptimModel(parameters, key=sys.argv[6])
+parameters['wheel_period']=24
+parameters['stage_duration'] = int(sys.argv[5])
+parameters['n_stages'] = int(sys.argv[1])
+parameters['n_stochastics'] = int(sys.argv[2])
+parameters['hydrogen_price'] = 5 #$/kg
+parameters['random_seed'] = int(sys.argv[4])
+parameters['relaxed_ramping'] = True
+parameters['vector_operating_duration'] = 1
+parameters['shipping_decision'] = 168
+
+model = OptimModel(parameters, key=sys.argv[8])
 
     
