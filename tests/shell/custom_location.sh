e1daa2ad706ed66cd08a32680ef318b5b8a024e2
Cameron Aldren <cameron.aldren19@imperial.ac.uk>
2025-05-01 10:54:52 +0100

    Added comments to the shell script.

--- tests/shell/custom_location.sh
+++ tests/shell/custom_location.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
+# This loads in the credentials for the Earthdata login
 script_dir=$(dirname "$0")
 if [ -f "$script_dir/_creds.sh" ]; then
     chmod +x "$script_dir/_creds.sh"
@@ -7,6 +8,7 @@ if [ -f "$script_dir/_creds.sh" ]; then
     echo "[INFO] Earthdata credentials loaded successfully"
 fi
 
+# These variables can be changed subject to the chosen location 
 vector="NH3"
 grid_connection="False"
 n_stages=3
@@ -20,5 +22,6 @@ optimality=1e-8
 mip_percentage=5
 random_seed=42
 
+# These two scripts are used to build the weather data and solve the model
 python tests/models/weather_build.py "$n_stages" "$n_branches" "$vector" "$random_seed" "$stage_duration" "$renewables" "$grid_connection" "$key"
 python tests/models/solve.py "$key" "$feasibility" "$optimality" "$mip_percentage" "$random_seed"
